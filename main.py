04a18d07a39f8011d4a8461115e50da3b9374a55
Paul Hayes <paul.hayes@entropedia.co.uk>
2021-08-10 11:24:03 +0100

    Added code to auto reconnect, if devices disconnects.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import serial.tools.list_ports
 from datetime import datetime
+from time import sleep
 import serial
 import atexit
 import flask
 from flask import Response
 from multiprocessing import Process, Manager
 
-ports = serial.tools.list_ports.comports()
 
 connections = list()
 
@@ -17,12 +17,24 @@ manager = Manager()
 messages = manager.list()
 max_msgs = 500
 
-for p in ports:
-    if p.device in device_blacklist:
-        continue
-    print("connecting to: ".format(p.device))
-    connection = serial.Serial(port=p.device)
-    connections.append(connection)
+def find_devices():
+    global connections
+    ports = serial.tools.list_ports.comports()
+    print("looking for devices")
+    for c in connections:
+        if c.is_open:
+            c.close()
+    connections = list()
+    for p in ports:
+        if p.device in device_blacklist:
+            continue
+        msg = ("connecting to: {0}".format(p.device))
+        print(msg)
+        messages.insert(0,msg)
+        connection = serial.Serial(port=p.device)
+        connections.append(connection)
+
+find_devices()
 
 def start_server():
     print("starting server");
@@ -49,19 +61,26 @@ server = Process(target=start_server)
 server.start()
 
 while True:
+    if len(connections) == 0:
+        sleep(1)
+        find_devices()
     for c in connections:
         #print("meep")
         #print( c.in_waiting )
-        if c.in_waiting>0:
-            dt = datetime.now().strftime("%H:%M %d/%m/%Y")
-            msg = "[{1} {2}] {0}".format( c.read(size=c.in_waiting).decode('utf-8').strip(), c.name, dt )
-            print( msg )
-            #help(c)
-            #f.write("[{0}]".format(c.device))
-            #f.write( msg )
-            messages.insert(0,msg)
-            if len(messages) > max_msgs:
-                messages.pop()
+        try:
+            if c.in_waiting>0:
+                dt = datetime.now().strftime("%H:%M %d/%m/%Y")
+                msg = "[{1} {2}] {0}".format( c.read(size=c.in_waiting).decode('utf-8').strip(), c.name, dt )
+                print( msg )
+                #help(c)
+                #f.write("[{0}]".format(c.device))
+                #f.write( msg )
+                messages.insert(0,msg)
+                if len(messages) > max_msgs:
+                    messages.pop()
+        except OSError:
+            messages.insert(0,"{0} disconnected".format(c.name))
+            find_devices()
             
 
 
